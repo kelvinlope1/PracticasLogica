e68afa02fe9bae412c8a88cbfcd98cca263a3dee
Kelvin Manuel Lopez <kelvinlopez060301@gmail.com>
2025-12-18 20:32:37 -0800

    agregamos la carpeta learning

--- ejercicios_logica.py
+++ ejercicios_logica.py
@@ -11,28 +11,28 @@ else:
 #determinar el numero mayor entre dos numeros ingresados por el usuario
 
 #controlador de errores 
-# try:
+try:
     
-#     #declaracion de variables para ingresar datos por el usuario
-#     ingresar_numero = float(input("Ingresar un primer valor:"))
-#     ingresar_numero2 = float(input("Ingresar un segundo valor:"))
-#     ingresar_numero3 = float(input("Ingresar un tercer valor:"))
+    #declaracion de variables para ingresar datos por el usuario
+    ingresar_numero = float(input("Ingresar un primer valor:"))
+    ingresar_numero2 = float(input("Ingresar un segundo valor:"))
+    ingresar_numero3 = float(input("Ingresar un tercer valor:"))
 
-#     #condicionales para determinar si los datos ingresados son iguales o mayores 
-#     if ingresar_numero == ingresar_numero2 == ingresar_numero3:
-#         print("Todos los numeros Son iguales")
-#     elif ingresar_numero > ingresar_numero2 and ingresar_numero > ingresar_numero3:
-#         print("El numero mayor es:", ingresar_numero)
-#     elif ingresar_numero2 > ingresar_numero and ingresar_numero2 > ingresar_numero3:
-#         print("El numero mayor es:", ingresar_numero2)
-#     elif ingresar_numero3 > ingresar_numero and ingresar_numero3 > ingresar_numero2:
-#         print("El numero mayor es:", ingresar_numero3)
-#     else:
-#         print("hay mas de un numero mayor")
+    #condicionales para determinar si los datos ingresados son iguales o mayores 
+    if ingresar_numero == ingresar_numero2 == ingresar_numero3:
+        print("Todos los numeros Son iguales")
+    elif ingresar_numero > ingresar_numero2 and ingresar_numero > ingresar_numero3:
+        print("El numero mayor es:", ingresar_numero)
+    elif ingresar_numero2 > ingresar_numero and ingresar_numero2 > ingresar_numero3:
+        print("El numero mayor es:", ingresar_numero2)
+    elif ingresar_numero3 > ingresar_numero and ingresar_numero3 > ingresar_numero2:
+        print("El numero mayor es:", ingresar_numero3)
+    else:
+        print("hay mas de un numero mayor")
         
-# # cierre del controlador de errores con mensaje si el usuario intenta ingresar datos que no sean numeros
-# except ValueError:
-#     print("Error: Solo se aceptan valor en numeros")
+# cierre del controlador de errores con mensaje si el usuario intenta ingresar datos que no sean numeros
+except ValueError:
+    print("Error: Solo se aceptan valor en numeros")
     
 valor_inicial = int(input("ingresa el valor inicial:"))
 Valor_suma = int(input("ingresar numero para sumar: "))
@@ -54,7 +54,7 @@ try:
         print(f"Total a pagar de entreda es {pagar_entreda}")
     elif edad >= 40:
         resultado =entreda * 0.005
-        pagar_entreda = 1000 - resultado
+        pagar_entreda = 1000 - resultad
         print("Puedes entrar pero con un descuento del 5%")
         print(f"Total a Pagar de entreda es {pagar_entreda}")
     elif edad > 18:
